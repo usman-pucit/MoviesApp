28d8904df368454fd9390c5680bfc79dcd1eb836
Muhammad Usman <m.usman@tdsoftware.de>
2023-03-17 19:58:17 +0100

    Initial project setup

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Muhammad Usman on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
